c847f7af01fb23b99fb29533ac3b43974393c0cb
fodisi <fodisi@users.noreply.github.com>
2018-06-29 20:58:00 -0400

    updated web trader app files

--- w5/exercises/tt/core/controllers/sell.py
+++ w5/exercises/tt/core/controllers/sell.py
@@ -3,7 +3,7 @@
 from flask import Blueprint, render_template, request, session
 
 from core.model.order import Order
-from core.model.user import User
+from core.model.holding import Holding
 
 
 sell_ctrl = Blueprint('sell', __name__, url_prefix='/sell')
@@ -11,21 +11,22 @@ sell_ctrl = Blueprint('sell', __name__, url_prefix='/sell')
 html_filename = 'sell.html'
 
 
-def sell(symbol, volume, username):
-    status = ''
-    error_detail = ''
+def __sell(symbol, volume, username):
+    status = None
+    error_detail = None
     try:
-        status = Order().buy(symbol, int(volume), username)
+        # Executes the order and stores the status (SUCCESS or NO_FUNDS)
+        status = Order().sell(symbol, int(volume), username)
+
+        # If user doesn't have enought holdings, sets error_details with available balance.
         if status == 'NO_FUNDS':
-            error_detail = User().get_current_balance(username)
+            hold_volume = Holding().get_holding_volume(username, symbol)
+            error_detail = {'symbol': symbol, 'hold_volume': hold_volume}
     except Exception as e:
         status = 'EXCEPTION'
         error_detail = e.args[0]
 
-    return render_template(html_filename,
-                           username=username,
-                           status=status,
-                           error_detail=error_detail)
+    return render_template(html_filename, status=status, error_detail=error_detail)
 
 
 @sell_ctrl.route('/', methods=['GET', 'POST'])
@@ -34,4 +35,4 @@ def show_sell():
     if request.method == 'GET':
         return render_template(html_filename, status='', error_detail='')
     else:
-        return sell(request.form['symbol'], request.form['volume'], session['user'])
+        return __sell(request.form['symbol'], request.form['volume'], session['user'])
