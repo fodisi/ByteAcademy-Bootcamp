238641884f99cfc356befa6e3daec852e46c1f49
fodisi <fodisi@users.noreply.github.com>
2018-06-29 20:57:20 -0400

    updated web trader app files

--- w5/exercises/tt/core/controllers/lookup.py
+++ w5/exercises/tt/core/controllers/lookup.py
@@ -2,12 +2,28 @@
 
 from flask import Blueprint, render_template, request
 
+from core.model.order import Order
 
 lookup_ctrl = Blueprint('lookup', __name__, url_prefix='/lookup')
 
 html_filename = 'lookup.html'
 
 
+def __lookup(company_name):
+    company = None
+    error = None
+    try:
+        symbol = Order().get_ticker_symbol(company_name)
+        company = {'name': company_name, 'symbol': symbol}
+    except Exception as e:
+        error = e.args[0]
+
+    return render_template(html_filename, company=company, error=error)
+
+
 @lookup_ctrl.route('/', methods=['GET', 'POST'])
 def show_lookup():
-    pass
+    if request.method == 'GET':
+        return render_template(html_filename)
+    else:
+        return __lookup(request.form['company'])
